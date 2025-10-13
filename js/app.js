0db0e87b731554bd38edfc383e043273360f7494
hermenegildoAng <hernandezhermenegildoangeldavi@gmail.com>
2025-10-13 11:44:43 -0700

    01 Load guitars

---
+++ js/app.js
@@ -0,0 +1,63 @@
+
+import { db } from './guitarras.js'
+
+/*console.log(db[0].id)
+console.log(db[10].nombre)
+console.log(db[5].imagen) */
+
+// for(let i = 0; i < db.length; i++){
+//     console.log(db[i].nombre)
+// }
+
+/*
+const saluda = (name) => {
+    console.log('hello' + name)
+}
+saluda('mundo') */ 
+
+//metodos de Arrays para Iterar
+db.forEach(
+    function(guitar){
+        console.log(guitar)
+    }
+)
+
+
+const creaCard = (guitar) => {
+    const div = document.createElement('div')
+    div.className = 'col-md-6 col-lg-4 my-4 row align-items-center'
+    const html = `
+                <div class="col-4">
+                    <img class="img-fluid" src="./img/${guitar.imagen}.jpg" alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">${guitar.nombre}</h3>
+                    <p>${guitar.descripcion}</p>
+                    <p class="fw-black text-primary fs-3">$${guitar.precio}</p>
+                    <button 
+                        data-id = "${guitar.id}"
+                        type="button"
+                        class="btn btn-dark w-100 "
+                    >Agregar al Carrito</button>
+                </div>`
+    div.innerHTML = html
+    return div
+}
+
+const container = document.querySelector('main div')
+
+const buttonClicked = (e) => {
+
+    if (e.target.classList.contains('btn')) {
+        const dataId = e.target.getAttribute('data-id')
+        console.log(db[Number(dataId) - 1])
+    }
+}
+
+db.forEach((guitar) => {
+    console.log(guitar.nombre)
+    container.appendChild(creaCard(guitar))
+}
+)
+
+container.addEventListener('click',buttonClicked)
